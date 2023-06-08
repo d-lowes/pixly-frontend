1f2e3539ebcfcfcec5bb7d92d5693207b3b17c36
Jakob <shavinski.jakob@gmail.com>
2023-06-08 12:35:02 -0700

    add ability to upload to canvas

--- src/UploadForm.js
+++ src/UploadForm.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useRef } from "react";
 import './UploadForm.css';
 
 function UploadForm() {
+  const canvasRef = useRef();
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const file = evt.target.files[0];
+
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext('2d');
+
+        // Set canvas dimensions to match the image
+        canvas.width = img.width;
+        canvas.height = img.height;
+
+        // Draw the image onto the canvas
+        ctx.drawImage(img, 0, 0);
+      };
+      img.src = event.target.result;
+    };
+
+    reader.readAsDataURL(file);
+  }
 
   function handleClick(evt) {
     // evt.preventDefault();
@@ -14,54 +37,25 @@ function UploadForm() {
     <div className="upload-img-container m-4">
       <div className="row">
         <div className="col-md-10 m-auto">
-          <div className="custom-file mb-3">
-            <input
-              type="file"
-              className="custom-file-input"
-              name="image"
-              id="upload-file"></input>
-            <label htmlFor="upload-file" className="custom-file-label">
-              Select image
-            </label>
-          </div>
-          <canvas id="canvas"></canvas>
-          <h4 className="text-center my-3">Filters</h4>
-          <div className="row mb-3 m-1">
-            <div className="col-md-3">
-              <button
-                className="filter-btn vintage-add btn btn-ig btn-block"
-                onClick={handleClick}>
-                Vintage
-              </button>
+          <form>
+            <div className="custom-file mb-3">
+              <input
+                type="file"
+                className="custom-file-input"
+                name="image"
+                id="upload-file"
+                onChange={handleSubmit}
+              />
+              <label htmlFor="upload-file" className="custom-file-label">
+                Select image
+              </label>
             </div>
-            <div className="col-md-3">
-              <button
-                className="filter-btn clarity-add btn btn-ig btn-block"
-                onClick={handleClick}>
-                Clarity
-              </button>
-            </div>
-            <div className="col-md-3">
-              <button
-                className="filter-btn hazy-days-add btn btn-ig btn-block"
-                onClick={handleClick}>
-                Hazy Days
-              </button>
-            </div>
-            <div className="col-md-3">
-              <button
-                className="filter-btn her-majesty-add btn btn-ig btn-block"
-                onClick={handleClick}>
-                Her Majesty
-              </button>
-            </div>
-          </div>
-          <div className="col-md-6">
-            <button className="remove-btn btn btn-danger"
-              onClick={handleClick}>
-              Remove Filters
-            </button>
-          </div>
+          </form>
+          <canvas
+            ref={canvasRef}
+            id="canvas"
+            style={{ width: '500px', height: '300px' }}>
+          </canvas>
         </div>
       </div>
     </div>
